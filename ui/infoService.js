89362cc43fa2626d0a640baf297cf5d83bd8cd81
josiah1888 <josiah.burchard@gmail.com>
2015-03-14 12:37:37 -0500

    created ui and service

---
+++ ui/infoService.js
@@ -0,0 +1,21 @@
+﻿(function() {
+    'use strict';
+
+    var app = angular.module('frogger');
+
+    app.factory('infoService', [function() {
+        var infoService = {
+            title: '',
+            player: '',
+            enemy: '',
+            safeZone: '',
+            road: '',
+            divider: '',
+            victoryZone: ''
+        };
+
+
+        return infoService;
+    }]);
+
+})();
