728aa5ce9f101a4b4e7911849df57a9c56bef301
Cory Bateman <splintercode.cb@gmail.com>
2015-03-15 13:32:42 -0500

    merge and firebase

--- ui/app.js
+++ ui/app.js
@@ -1,7 +1,7 @@
 ﻿(function() {
     'use strict';
 
-    var app = angular.module('frogger', []);
+    var app = angular.module('frogger', ['firebase']);
     
     app.controller('frogger', ['infoService', '$location', function(infoService, $location) {
         var vm = this;
